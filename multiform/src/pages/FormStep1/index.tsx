efef0080eab32afcbae13ff510549e21dd41b865
Wesley-Maia <maia.weol@gmail.com>
2021-12-16 21:55:08 -0300

    Project-5: finished (part 2)

--- multiform/src/pages/FormStep1/index.tsx
+++ multiform/src/pages/FormStep1/index.tsx
@@ -1,13 +1,34 @@
 import { useHistory } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
 import { Theme } from '../../components/Theme';
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep1 = () => {
 
   const history = useHistory();
+  const {state, dispatch} = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1,
+    });
+  },[])
 
   const handleNextStep = () => {
-    history.push('/step2');
+    if(state.name !== '') {
+      history.push('/step2');
+    } else {
+      alert("Preencha os dados.")
+    }
+  }
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value,
+    });
   }
 
   return (
@@ -24,6 +45,8 @@ export const FormStep1 = () => {
           <input 
             type="text"
             autoFocus={true}
+            value={state.name}
+            onChange={handleNameChange}
           />
         </label>
         <button onClick={handleNextStep}>Próximo</button>
